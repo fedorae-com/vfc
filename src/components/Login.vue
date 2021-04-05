25e80cae1bfefed214f03a16f05a2af34866a4bb
aeorae <>
2021-04-05 16:46:09 -0700

    Added Login Component

---
+++ src/components/Login.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center" dense>
+          <v-col cols="12" sm="8" md="4" lg="4">
+            <v-card elevation="0">
+              <a href="https://edu-fedorae.netlify.app" name="Fedorae Education" title="Fedorae Education" target="_blank">
+                <v-img src="@/assets/logo.png" alt="Fedorae Education Log" contain height="200"></v-img>
+              </a>
+              <v-card-text>
+                <v-form>
+                  <v-text-field label="Enter your email" name="email" prepend-inner-icon="mdi-mail" type="email" class="rounded-0" outlined></v-text-field>
+                  <v-text-field label="Enter your password" name="password" prepend-inner-icon="mdi-lock" type="password" suffix="| Forgot?" class="rounded-0" outlined></v-text-field>
+                  <v-btn class="rounded-0" color="#000000" x-large block dark>Login</v-btn>
+                  <v-card-actions class="text--secondary">
+                    <v-checkbox color="#000000" label="Remember me"></v-checkbox>
+                    <v-spacer></v-spacer>
+                    <!-- <router-link :to="{ name: 'SignUp' }">Sign Up</router-link> -->
+                    No account? <a href="#" class="pl-2" style="color: #000000">Sign Up</a>
+                  </v-card-actions>
+                </v-form>
+              </v-card-text>
+              <v-card-actions class="ml-6 mr-6 text-center">
+                <p>By continuing, you agree to Fedorae Education's <a href="#" class="pl-2" style="color: #000000">Policy</a> and <a href="#" class="pl-2" style="color: #000000">Terms of Use</a></p>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
