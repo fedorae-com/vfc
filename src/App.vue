6d27a3567ad81b2481ee03e82948cb1e0e5d9d37
Fedorae <services.fedorae@gmail.com>
2021-06-08 10:37:51 +0100

    Update ~ added nav tabs

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
-  <div>
-    <Login v-if="false" />
-    <ForgotPassword v-if="false" />
-    <Register />
-  </div>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-tabs>
+          <v-tab @click="login = true">Login</v-tab>
+          <v-tab @click="register = true; login = false; forgotpassword = false;">Register</v-tab>
+          <v-tab @click="forgotpassword = true; register = true; login = false">Forgot Password</v-tab>
+        </v-tabs>
+        <div>
+          <Login v-if="login" />
+          <ForgotPassword v-if="forgotpassword" />
+          <Register v-if="register" />
+        </div>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 
@@ -14,6 +25,13 @@ import Register from "@/components/Register"
 
 export default {
   name: 'App',
+  data() {
+    return {
+      login: true,
+      register: false,
+      forgotpassword: false
+    }
+  },
   components: {
     Login,
     ForgotPassword,
