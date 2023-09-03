b812973e4cc8e76c596572d1aa56ed7f74695781
Yisrae <yisrae@fedorae>
2023-09-03 11:24:48 +0100

    refactor(playground): change playground preview component

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,5 +1,12 @@
 <template>
     <v-app>
-        <VFCImageUploadPreview />
+        <v-container class="fill-height" fluid>
+            <v-row align="center" justify="center" dense>
+                <v-col cols="12" md="8"> </v-col>
+                <v-col cols="12" md="4">
+                    <VFCLogin />
+                </v-col>
+            </v-row>
+        </v-container>
     </v-app>
 </template>
